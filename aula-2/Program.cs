de45859470c21fcd852cb0a2c741c515615ef12b
mosiah-andrade <mosiahassuncao@gmail.com>
2025-08-29 22:20:03 -0300

    update and create class

--- aula-2/Program.cs
+++ aula-2/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using aula_2.models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "João";
+pessoa1.Idade = 25;
+pessoa1.Apresentar();
